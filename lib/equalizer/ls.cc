b6d35ffe7234c996d04f766a048d7b80b383b3cf
thomas <thomas.dutrannois@multitel.be>
2025-01-03 09:28:24 +0100

    equalizer LS : compute CSI based on an average of all four LTS

--- lib/equalizer/ls.cc
+++ lib/equalizer/ls.cc
@@ -27,27 +27,54 @@ void ls::equalize(gr_complex* in,
                   uint8_t* bits,
                   std::shared_ptr<gr::digital::constellation> mod)
 {
-
+    /*
     if (n == 0) {
         std::memcpy(d_H, in, SAMPLES_PER_OFDM_SYMBOL * sizeof(gr_complex));
 
-    } else if (n == 1) {
+    }
+    else if( n <= 2){
+
+        for (int i = 0; i < SAMPLES_PER_OFDM_SYMBOL; i++) {
+            if ((i == 16) || (i < 3) || (i > 29)) { //dividing by two gets you the dead DC pilot subcarrier, the other indices correspond to dead lower and upper subcarriers. 3 dead lower ones, 2 dead upper ones
+                continue;
+            }
+            d_H[i] += in[i];
+        }
+    }
+    else if (n == 3) {
+        //debug
+        //std::cout << "H : ";
+
         double signal = 0;
         double noise = 0;
         for (int i = 0; i < SAMPLES_PER_OFDM_SYMBOL; i++) {
             if ((i == 16) || (i < 3) || (i > 29)) { //dividing by two gets you the dead DC pilot subcarrier, the other indices correspond to dead lower and upper subcarriers. 3 dead lower ones, 2 dead upper ones
                 continue;
             }
-            noise += std::pow(std::abs(d_H[i] - in[i]), 2);
-            signal += std::pow(std::abs(d_H[i] + in[i]), 2);
+            //TODO : add snr computation
+            //noise += std::pow(std::abs(d_H[i] - in[i]), 2);
+            //signal += std::pow(std::abs(d_H[i] + in[i]), 2);
             d_H[i] += in[i];
-            d_H[i] /= LONG[i] * gr_complex(2, 0);
+            d_H[i] /= LONG[i] * gr_complex(4, 0);
+            
+            //debug
+            //std::cout << abs(d_H[i])  << std::endl;
         }
 
-        d_snr = 10 * std::log10(signal / noise / 2);
+        //d_snr = 10 * std::log10(signal / noise / 2);
 
-    } else {
-
+    }
+    */
+    if(n < NUM_OFDM_SYMBOLS_IN_LTF1){//if we are in LTF1
+        for (int i = 0; i < SAMPLES_PER_OFDM_SYMBOL; i++) {
+            if ((i == 16) || (i < 3) || (i > 29)) {
+                continue;//skip if dead subcarrier
+            }
+            d_H[i] *= gr_complex((float) n / (n + 1), 0);//rescale current mean iaw previous and current mean sizes
+            d_H[i] += (in[i] / LONG[i]) / gr_complex(n + 1, 0);//add latest channel estimate, scaled to current mean size
+        }
+    }
+    else {
         int c = 0;
         for (int i = 0; i < SAMPLES_PER_OFDM_SYMBOL; i++) {
             if ((i == PILOT1_INDEX) || (i == 16) || (i == PILOT2_INDEX) ||
